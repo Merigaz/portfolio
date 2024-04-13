4ef2e8687ad17b8623441e64a8ab9948dd1be3a6
Merigaz <Davidpdavila@gmail.com>
2024-04-12 22:10:43 -0500

    creating middleware and i18n config file

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,1 +1,16 @@
-export default function Home() {}
+import { Locale } from "../../i18n.config";
+
+export default async function Home({
+  params: { lang },
+}: {
+  params: { lang: Locale };
+}) {
+  return (
+    <section className="py-24">
+      <div className="container">
+        <h1 className="text-3xl font-bold">palote</h1>
+        <p className="text-gray-500">palo</p>
+      </div>
+    </section>
+  );
+}
